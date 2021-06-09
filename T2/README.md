1ab13c81e34ea384d6281abbfe368cd273a1d23a
FelTavoni <56005905+FelTavoni@users.noreply.github.com>
2021-06-09 11:37:52 -0300

    Localização de objetos operante
    - O programa consegue agora localizar os objetos em uma imagem;
    - READMe com instruções de execução atualizados.

--- T2/README.md
+++ T2/README.md
@@ -2,6 +2,16 @@
 
 O trabalho 2 da disciplina de *Processamento Digital de Imagens* consiste em implementar um filtro capaz de localizar um objeto com iluminação diferente da imagem.
 
+## Como executar
+
+- Via linha de comando
+
+A execução da localização de objetos deve seguir a seguinte forma:
+
+`python search_object.py <caminho-da-imagem> <caminho-do-objeto> <claro | escuro>`
+
+A tag claro ou escuro ajuda a obter uma melhor resposta, visto que pode haver confusão caso se escureça/clarifique demais a imagem.
+
 ### Ferramentas Utilizadas
 
 - [Python v3.9.5](https://www.python.org/)
--- T2/search_object.py
+++ T2/search_object.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy.signal
 import matplotlib.pyplot as plt
 
-def search_object(img_path, obj_path):
+def search_object(img_path, obj_path, obj_ilum):
 	"""
     Desc.: Procura determinado objeto em uma imagem, o qual pode varia em sua luminosidade.
     I: O caminho de uma imagem (img_path) e o caminho para a imagem de um objeto (obj_path).
@@ -19,17 +19,50 @@ def search_object(img_path, obj_path):
 
 	# print(img.shape)
 	# print(obj.shape)
+	# plt.imshow(img, 'gray')
+	# plt.show()
+	# plt.imshow(obj, 'gray')
+	# plt.show()
 
-	# Cálculo da diferença quadrática utilizando correlação cruzada.
-	diff = square_correlation(img, obj)
-	plt.imshow(diff, 'gray')
-	plt.show()
+	"""
+	É necessário que seja especificado se o objeto está mais escuro/claro que a imagem. Isso porque pode acontecer de,
+		caso teste todos os valores gamas, escurecer a imagem demais pode causar de ela coincidir com algum elemento
+		mais escuro que a imagem, o que não é o desejado. Assim, se desejamos clarificar a imagem, utilizamos gamas
+		maiores que 1, enquanto para escurecer, utilizamos gamas menores que 1.
+	"""
+	global_pos = (0,0)
+	global_min = np.Inf
+	if obj_ilum == "claro":
+		gamma_values = np.linspace(1,2,11)
+	elif obj_ilum == "escuro":
+		gamma_values = np.linspace(0,1,11)
+	else:
+		print("Não disponível...")
+		exit()
 
-	# Localizando a posição mínima na imagem.
-	pos = find_minimum(diff)
+	"""
+	Para cada faixa gamma, será calculada um novo obj mais escuro/claro, e então efetuada a correlação cruzada
+		das diferenças quadráticas. Aquele que possuir menor diferença será o ideal para localizá-lo na imagem.
+	"""
+
+	print(gamma_values)
+	for gamma in gamma_values:
+		gamma_obj = power_law_tranformation(obj, gamma)
+		diff = square_correlation(img, gamma_obj)
+		min, pos = find_minimum(diff)
+		if min < global_min:
+			global_min = min
+			global_pos = pos
+		print("Gamma: ", gamma)
+		print(min, pos)
+		print(global_pos)
+
+	# Exibindo resultados da diferença
+	# plt.imshow(diff, 'gray')
+	# plt.show()
 
 	# Desenhando o retângulo para localização da imagem e apresentando o resultado final.
-	foundObj = draw_rectangle(img, pos, obj.shape)
+	foundObj = draw_rectangle(img, global_pos, obj.shape)
 	plt.imshow(foundObj, 'gray')
 	plt.show()
 
@@ -80,11 +113,21 @@ def find_minimum(img):
     		if valor<=menor_valor:
     			menor_valor = valor
     			indice_menor_valor = (row, col)
-    			print(menor_valor, indice_menor_valor)
 
-    return indice_menor_valor
+    return menor_valor, indice_menor_valor
+
+def power_law_tranformation(obj, gamma):
+	"""
+	Desc: Transforma uma imagem segundo a lei da potência (gamma).
+	I: Uma imagem.
+	O: O resultado da aplicação da transf. gamma ( s = c*r^y ) na imagem, com const = 1.
+	"""
+	gamma_obj = np.zeros(obj.shape, dtype=np.uint8)
+	gamma_obj = 255*(obj/255)**gamma
+	return gamma_obj
 
 if __name__ == "__main__":
 	img_path = sys.argv[1]
 	obj_path = sys.argv[2]
-	search_object(img_path, obj_path)
+	obj_ilum = sys.argv[3]
+	search_object(img_path, obj_path, obj_ilum)
