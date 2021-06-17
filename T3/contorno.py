51c556865d091e75ab131b7e778abf464a93b2a8
FelTavoni <56005905+FelTavoni@users.noreply.github.com>
2021-06-17 10:44:14 -0300

    Tentativa frustrada de cálculo do contorno.

--- T3/contorno.py
+++ T3/contorno.py
@@ -2,9 +2,55 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+def threshold_otsu(img):
+    '''
+    Calcula e aplica o limiar de Otsu utilizando o histograma da imagem
+    '''
+    
+    bins = range(0, 257)
+    hist, _ = np.histogram(img, bins)
+    
+    num_pixels = img.shape[0]*img.shape[1] 
+    sum_img = np.sum(img)
+    m_G = sum_img/num_pixels
+    max_sigma_I = -1
+    
+    sum_back = 0
+    num_back = 0
+    for threshold in range(0, 256):
+        num_back = num_back + hist[threshold]  # Número de pixels com valor menor que threshold
+        sum_back = sum_back + threshold*hist[threshold]  # Soma dos valores de pixel background
+
+        num_fore = num_pixels - num_back
+        sum_fore = sum_img - sum_back
+        
+        if num_back == 0 or num_fore == 0:
+            continue
+        
+        P_back = num_back/num_pixels
+        P_fore = num_fore/num_pixels
+        m_back = sum_back/num_back   
+        m_fore = sum_fore/num_fore    
+        
+        sigma_I = P_back*(m_back-m_G)**2 + P_fore*(m_fore-m_G)**2
+        
+        if sigma_I > max_sigma_I:
+            max_sigma_I = sigma_I
+            best_threshold = threshold
+
+    img_bin = np.zeros((img.shape[0], img.shape[1]))
+
+    for row in range(img.shape[0]):
+        for col in range(img.shape[1]):
+            if img[row, col] > best_threshold:
+                img_bin[row, col] = 1
+    
+    return img_bin
+
 def image_contour(img):
-    '''Obtém o contorno paramétrico de um objeto contido
-       no array img.'''
+    '''
+    Obtém o contorno paramétrico de um objeto contido no array img.
+    '''
     
     # Mapeamento utilizado para encontrar o vizinho inicial a ser
     # buscado na próxima iteração dado o vizinho do ponto atual
@@ -16,8 +62,7 @@ def image_contour(img):
     # o ponto atual
     neighbor_map = [7, 7, 1, 1, 3, 3, 5, 5]
     
-    # Adiciona 0 ao redor da imagem para evitar pontos 
-    # do objeto tocando a borda
+    # Adiciona 0 ao redor da imagem para evitar pontos do objeto tocando a borda
     img_pad = np.pad(img, 1, mode='constant')
     
     num_rows, num_cols = img_pad.shape
@@ -34,11 +79,9 @@ def image_contour(img):
     contour = [curr_point]     # Pontos do contorno
     starting_index = 2         # Índice do vizinho inicial a ser verificado
     while True:
-        next_point, last_index = get_next_point(img_pad, curr_point, 
-                                                    starting_index)
+        next_point, last_index = get_next_point(img_pad, curr_point, starting_index)
 
-        # Novo índice do vizinho inicial baseado no último
-        # índice buscado
+        # Novo índice do vizinho inicial baseado no último índice buscado
         starting_index = neighbor_map[last_index]
         
         # Critério de parada. Se o ponto adicionado na iteração anterior (contour[-1])
@@ -53,8 +96,7 @@ def image_contour(img):
         contour.append(next_point)
         curr_point = next_point
         
-    # Subtrai 1 de cada ponto pois o contorno foi encontrado para a
-    # imagem preenchida com 0 na borda
+    # Subtrai 1 de cada ponto pois o contorno foi encontrado para a imagem preenchida com 0 na borda
     for point_index, point in enumerate(contour):
         contour[point_index] = (point[0]-1, point[1]-1)
         
@@ -81,18 +123,59 @@ def get_next_point(img, curr_point, starting_index):
         else:
             curr_index = (curr_index+1)%8
 
+def curvatura(cont):
+    '''
+    Cálculo da curvatura ao longo do contorno de objetos.
+    '''
+    x = y = k = []
+    # Separando os eixos x e y.
+    for p in cont:
+        x.append(p[0])
+        y.append(p[1])
+
+    # Cálculando as derivadas (primeira e segunda ordem).
+    dx = np.diff(x)
+    dy = np.diff(y)
+    d2x = np.diff(x, 2)
+    d2y = np.diff(y, 2)
+
+    # Iniciando o cálculo da curvatura segundo a fórmula de ___ .
+    # Vale lembrar que a derivada 'encurta' os valores, já que cálcula a variação.
+    for t in range(len(d2x)):
+        if (((dx[t]**2)+(dy[t]**2))**(3/2)) == 0:
+            k[t] = 0
+        else:
+            k[t] = ((dx[t]*d2y[t])-(d2x[t]*dy[t]))/(((dx[t]**2)+(dy[t]**2))**(3/2))
+
+    t = range(0, len(d2x)+2)
+
+    plt.plot(t, k)
+    plt.show()
+
 if __name__ == "__main__":
+    # Lendo a imagem e a mostrando.
     img_path = sys.argv[1]
     img = plt.imread(img_path)
     plt.imshow(img, 'gray')
     plt.show()
-    img = img > 0
+    print(np.unique(img))
+
+    # Calculando a limiarização de Otsu para apenas 2 únicos valores.
+    new_img = threshold_otsu(img)
+    plt.imshow(new_img, 'gray')
+    plt.show()
+    print(np.unique(new_img))
+
+    # Cálculando o contorno paramétrico da imagem.
+    img = (img > 0).astype(np.uint8)
     cont = image_contour(img)
     # Transforma o contorno em um array numpy
     cont = np.array(cont)
     plt.figure()
     plt.subplot(111, aspect='equal')
-    # Plota os pontos de contorno, precisamos inverter os valores em y porque,
-    # na imagem, a origem está no ponto superior esquerdo
+    # Plota os pontos de contorno, precisamos inverter os valores em y porque, na imagem, a origem está no ponto superior esquerdo
     plt.plot(cont[:,1], img.shape[0]-cont[:,0])
     plt.show()
+
+    # Dado o contorno, cálcular a curvatura ao longo do contorno.
+    curvatura(cont)
